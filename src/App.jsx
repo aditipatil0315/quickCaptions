fb18d60ac3fa29f1315e8fd797f6bddc4532fb0f
Aditi Patil <aditiprakashpatil7@gmail.com>
2026-01-04 22:22:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-pink-500">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
